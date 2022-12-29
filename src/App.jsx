5701967690aee8ab73794b64662ede98b99c773c
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2022-12-29 15:13:23 +0600

    front-page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { useState } from 'react'
+
+export default function App() {
+
+  return (
+    <div className="main">
+      <div className="design-one"></div>
+      <div className="design-two"></div>
+      <h1 className="title">Quizzical</h1>
+      <p className="description">Test your general knowledge skills by taking a quiz!</p>
+      <button className="btn-style">Start Quiz</button>
+    </div>
+  )
+}
